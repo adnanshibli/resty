1d97063bb0c60988eed4ffbe52a7ef5d40f1dfc8
adnanshibli <adnannajem.24.95@gmail.com>
2021-02-03 09:28:22 +0200

    lab2done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import Header from './Header.js';
-import Form from './Form.js';
-import Footer from './Footer.js';
-import './App.css';
+import Header from './components/Header.js';
+import Form from './components/Form.js';
+import Footer from './components/Footer.js';
+import Results from './components/Results.js';
+import React from 'react';
+import react from 'react';
+// import './App.css';
+
+class App extends react.Component{
+  constructor (props){
+    super(props);
+    this.state ={
+      count :0,
+      results: [],
+      headers:[]
+    }
+  }
+  handleForm =(data)=> {
+    console.log('data ------> before setState', data)
+    let count = data.body.count ;
+    let results = data.body;
+    let headers = data.headers;
+    this.setState({count : count, 
+    results: results,
+  headers:headers});
+  
+ 
+}
 
 
-function App() {
+render() {
   return (
     <>
-      <Header/>
-      <Form/>
-      <Footer/>
+      <Header />
+      <Form handler={this.handleForm}/>
+      <Results count={this.state.count} results={this.state.results} headers={this.state.headers} />
+      <Footer />
     </>
-  );
+  )
+}
 }
 
 export default App;
